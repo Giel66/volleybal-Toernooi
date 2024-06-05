b630347ef5c63f575c95e12a3e85de0b454c2c04
imsalehjr <166502402+imsalehjr@users.noreply.github.com>
2024-06-05 12:17:50 +0200

    Update hoofdpagina.php

--- volleybal toernooi/pages/hoofdpagina.php
+++ volleybal toernooi/pages/hoofdpagina.php
@@ -5,13 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welkom op de site van Volleyball</title>
     <link rel="stylesheet" href="stylesheets/hoofdpagina.css">
-
 </head>
 <body>
-
     <main>
         <div class="info-container">
-        <h1>Welkom op de site van Volleyball</h1>
+            <h1>Welkom op de site van Volleyball</h1>
             <p>
                 Hartelijk welkom op de officiële website van Volleyball! Hier vind je uitgebreide informatie over onze teams, de actuele klassementen, de puntentelling, veldindelingen en het volledige wedstrijdschema. Onze website is de perfecte plek voor zowel fans als spelers om alles te weten te komen over de sport en op de hoogte te blijven van de laatste ontwikkelingen en wedstrijden.
             </p>
@@ -22,8 +20,6 @@
                 Ga verder met de registratie of log in via de onderstaande knoppen. We hopen dat je veel plezier beleeft aan het verkennen van onze website en dat je geïnspireerd raakt door de passie en het enthousiasme voor volleyball.
             </p>
         </div>
- 
     </main>
-
 </body>
 </html>
